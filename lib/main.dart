8c376bc9018b1d21c99944369ff6479dfb9fcb71
Ibrahim Mubarok <mubarokibrahim@gmail>
2022-01-31 13:16:20 +0700

    Belajar bikin list view

--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,49 @@ import 'package:belajar_layouting/bottom_app_bar.dart';
 
 void main() => runApp(MaterialApp(
       theme: ThemeData(primarySwatch: Colors.deepPurple),
-      home: const HomePage(),
+      home: HomePage(),
       debugShowCheckedModeBanner: false,
     ));
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  List<Widget> widgets = [];
+
+  _HomePageState() {
+    for (int i = 0; i < 15; i++) {
+      widgets.add(Padding(
+        padding: EdgeInsets.all(16),
+        child: const Text("New Game Available",
+            style: TextStyle(fontWeight: FontWeight.w400, letterSpacing: 2.0)),
+      ));
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(title: const Text('Belajar Layouting')),
         bottomNavigationBar: const MyBottomNav(),
         body: Container(
-          child: ListView(
-            children: <Widget>[
-              const SizedBox(height: 16.0),
-              const SizedBox(height: 16.0),
-              const SizedBox(height: 16.0),
-              const SizedBox(height: 16.0),
-            ],
+          color: const Color(0xFFEFF4F8),
+          child: Center(
+            child: ListView(
+              children: <Widget>[
+                const Center(
+                  child: const Text(
+                    'GAME STORE',
+                    style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w700),
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: widgets
+                )
+              ],
+            ),
           ),
         ));
   }
